edaff3b5c68348ac85bdf1d527e59b3205e2adeb
vagrant <vagrant@vagrant.machine.puphpet>
2016-12-02 17:48:47 -0200

    Starting implementing the search for podcasts

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from flask import redirect, request, Response, send_from_directory
-import ujson
+from flask import jsonify
+from flask import redirect, request, send_from_directory
 from flask import render_template
 
 from flask_compress import Compress
@@ -11,6 +11,8 @@ from flask_caching import Cache
 from werkzeug.exceptions import abort
 from werkzeug.datastructures import Headers
 
+from server.task import search_term
+
 
 # Flask-Compress
 compress = Compress()
@@ -18,24 +20,26 @@ compress = Compress()
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 # Flask Application
 app = Flask(__name__)
-# Compressing static files integration!
+# Compressing to gzip integration!
 compress.init_app(app)
 # Caching the views integration!
 cache.init_app(app)
 
-# configs
+# CONFIGURATION
 # ------------------------------------------
 app.config['DEBUG'] = True
 app.config['COMPRESS_LEVEL'] = 9
 app.config['ASSETS_DEBUG'] = True
 
 
+# DEFAULT ROUTE, ALWAYS PRINT HOME SCREEN... THIS IS A REACT APP
 @app.route('/')
 @cache.cached(timeout=1)
 def index():
     return render_template('index.html')
 
 
+# SUB PATHS INSIDE REACT APP
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
@@ -43,6 +47,9 @@ def catch_all(path):
         return render_template('index.html')
     return abort(404)
 
+# -----------------------------------------------------
+# START APPS API!
+# -----------------------------------------------------
 
 @app.route("/messages")
 def messages():
@@ -51,17 +58,16 @@ def messages():
     response.mimetype = 'application/json'
     return response
 
-
 @app.route("/search/<term>")
 def search(term):
-    data = ujson.dumps({ "abc": True })
-    headers = Headers()
-    headers['Content-Encoding'] = 'gzip'
-    response = Response(response=data, status=200, headers=headers, mimetype="application/json")
-    return response
+    return jsonify(search_term(term))
+
+# -----------------------------------------------------
+# END: APPS API!
+# -----------------------------------------------------
 
 
-
+# STATIC ROUTES TO SERVER SOME FILES!
 @app.route('/robots.txt')
 @app.route('/humans.txt')
 @app.route('/sitemap.xml')
@@ -71,5 +77,6 @@ def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
 
+# MAIN APP START!
 if __name__ == "__main__":
     app.run()
